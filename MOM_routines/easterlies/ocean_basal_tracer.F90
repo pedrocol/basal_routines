6cf29671593134879ed544502e200e1dddf0a73e
pedrocol <colombo_pedro@hotmail.com>
2023-03-29 14:15:34 -0300

    up

--- MOM_routines/easterlies/ocean_basal_tracer.F90
+++ MOM_routines/easterlies/ocean_basal_tracer.F90
@@ -657,11 +657,10 @@ subroutine basal_tracer_source_1(Time, Time_steps, Thickness, T_prog, basal_i,di
                     if ( misfkt(i,j) > 1 ) tracernew(k) = T_prog(n)%field(i,j,k,tau)
                     T_prog(nn)%wrk1(i,j,k) = (tracernew(k)*(Thickness%rho_dzt(i,j,k,tau)+fwfisf(i,j)*dtime) -&
                                        T_prog(n)%field(i,j,k,tau)*Thickness%rho_dzt(i,j,k,tau))/dtime
-
                     do k=misfkt(i,j),misfkb(i,j)
                        T_prog(nn)%wrk1(i,j,k) = Thickness%rho_dzt(i,j,k,tau)*(tracernew(k) - T_prog(nn)%field(i,j,k,tau))/dtime !Tendency
                     enddo
-                 endif !tracers
+                 endif !gade
 
                  !Update tendency
                  k=1
@@ -669,7 +668,7 @@ subroutine basal_tracer_source_1(Time, Time_steps, Thickness, T_prog, basal_i,di
                  do k=misfkt(i,j),misfkb(i,j)
                     T_prog(nn)%th_tendency(i,j,k) = T_prog(nn)%th_tendency(i,j,k) + T_prog(nn)%wrk1(i,j,k)
                  enddo
-           
+                 PRINT *, 'hola'
               elseif (gade_line == 1 ) then
 
                  zextra=0.0
