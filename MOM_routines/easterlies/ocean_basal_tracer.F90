a3fb518d45b9448a53244a2205b5af93647819c4
pedrocol <colombo_pedro@hotmail.com>
2023-03-29 23:23:22 -0300

    up

--- MOM_routines/easterlies/ocean_basal_tracer.F90
+++ MOM_routines/easterlies/ocean_basal_tracer.F90
@@ -626,6 +626,7 @@ subroutine basal_tracer_source_1(Time, Time_steps, Thickness, T_prog, basal_i,di
                        tbasal_sum = tbasal_sum + tbasal*delta(k)
                        wrk2(i,j,k) = tracernew(k) - T_prog(nn)%field(i,j,k,tau) !Delta S
                     enddo
+                    if ( trim(T_prog(nn)%name) == 'salt' ) PRINT *, i,j,k,fwfisf(i,j),delta(k),basal3d(i,j,k),zinsert,T_prog(nn)%field(i,j,k,tau),tracernew(k)
 
                     T_prog(nn)%tbasal(i,j) = tbasal_sum !average for ocean_diagnostics
 
