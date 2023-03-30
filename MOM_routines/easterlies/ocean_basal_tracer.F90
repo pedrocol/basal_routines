c1b82bc033bf2cdbdf04d33b50bd524e368129a2
pedrocol <colombo_pedro@hotmail.com>
2023-03-30 14:44:49 -0300

    up

--- MOM_routines/easterlies/ocean_basal_tracer.F90
+++ MOM_routines/easterlies/ocean_basal_tracer.F90
@@ -583,6 +583,7 @@ subroutine basal_tracer_source_1(Time, Time_steps, Thickness, T_prog, basal_i,di
            depth       = min(Grd%ht(i,j),mininsertiondepth) !min between bathy and value
            misfkt(i,j) = min(Grd%kmt(i,j),floor(frac_index(depth,Grd%zw))) !min(mbathy,
            misfkt(i,j) = max(1,misfkt(i,j))
+           misfkt(i,j) = 1
            !maxinsertiondepth = Grd%zt(misfkb(i,j))
            maxinsertiondepth = misfzb(i,j)
            depth       = min(Grd%ht(i,j),maxinsertiondepth)     ! be sure not to discharge river content into rock, ht = ocean topography
